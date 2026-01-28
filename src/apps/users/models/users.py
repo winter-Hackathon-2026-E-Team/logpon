431014f571921dd3e23d381c4136401be0c35a93
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2026-01-28 23:43:42 +0900

    usersモデル作成、マイグレーション

--- src/apps/users/models/users.py
+++ src/apps/users/models/users.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
-# Create your models here.
+class User(AbstractUser):
+    class Meta:
+        db_table = 'users'
+    
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(max_length=255, validators=[username_validator],)
+    password = models.CharField(max_length=255,)
+    email = models.EmailField(blank=False, unique=True,)
+    # is_staff, is_activeはAbstractUserより継承
+    created_at = models.DateTimeField(auto_now_add=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
+
+    USERNAME_FIELD = 'email' # このテーブルのレコードを一意に識別
+    REQUIRED_FIELDS = ['username'] # スーパーユーザー作成時に入力する
+
+    def __str__(self) -> str:
+        return self.email
--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -131,3 +131,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else 
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
