77a84a8b37afa145e46c68dad254321226522009
NRT <xkr1470@gmail.com>
2026-02-25 02:30:57 +0900

    インフラ本番環境_20260225修正

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -36,46 +36,46 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # Application definition
 
 INSTALLED_APPS = [
-            'django.contrib.admin',
-                'django.contrib.auth',
-                    'django.contrib.contenttypes',
-                        'django.contrib.sessions',
-                            'django.contrib.messages',
-                                'django.contrib.staticfiles',
-                                    'django_bootstrap5',
-                                        'apps.users',
-                                            'apps.runs',
-                                                'apps.timers',
-                                                    'apps.programs',
-                                                    ]
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_bootstrap5',
+    'apps.users',
+    'apps.runs',
+    'apps.timers',
+    'apps.programs',
+    ]
 
 MIDDLEWARE = [
     'config.middleware.health_check.HealthCheckMiddleware',
-            'django.middleware.security.SecurityMiddleware',
-                'django.contrib.sessions.middleware.SessionMiddleware',
-                    'django.middleware.common.CommonMiddleware',
-                        'django.middleware.csrf.CsrfViewMiddleware',
-                            'django.contrib.auth.middleware.AuthenticationMiddleware',
-                                'django.contrib.messages.middleware.MessageMiddleware',
-                                    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-                                    ]
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
 
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
-            {
-                        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                                'DIRS': [BASE_DIR / 'templates'],
-                                        'APP_DIRS': True,
-                                                'OPTIONS': {
-                                                                'context_processors': [
-                                                                                    'django.template.context_processors.request',
-                                                                                                    'django.contrib.auth.context_processors.auth',
-                                                                                                                    'django.contrib.messages.context_processors.messages',
-                                                                                                                                ],
-                                                                        },
-                                                    },
-            ]
+        {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
@@ -84,37 +84,37 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-            'default': {
-                        'ENGINE': 'django.db.backends.mysql',
-                                'NAME': os.getenv('MYSQL_DATABASE'),
-                                        'USER': os.getenv('MYSQL_USER'),
-                                                'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-                                                        'HOST': os.getenv('MYSQL_HOST'),
-                                                                'PORT': os.getenv('MYSQL_PORT', '3306'),  # ← 修正: 環境変数から取得
-                                                                        'OPTIONS': {
-                                                                                        'charset': 'utf8mb4',
-                                                                                                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                                                                                                            },
-                                                                            }
-            }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),  # ← 修正: 環境変数から取得
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-            {
-                        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-                            },
-                {
-                            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-                                },
-                    {
-                                'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-                                    },
-                        {
-                                    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-                                        },
-                        ]
+        {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
 
 
 # Internationalization
