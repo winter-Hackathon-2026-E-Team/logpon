6fa752c2cf53d6a1c00b55eddc48d9b59081b007
hiro_53 <194663299+hiro-53-RareTECH@users.noreply.github.com>
2026-02-24 19:50:07 +0900

    パスワード変更処理を修正

--- src/apps/users/views/profile/profile_password.py
+++ src/apps/users/views/profile/profile_password.py
@@ -1,7 +1,15 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views import View
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ProfilePasswordView(LoginRequiredMixin, View):
-    def post(self, request, *args, **kwargs):
-        pass
+class ProfilePasswordView(LoginRequiredMixin, SuccessMessageMixin, PasswordChangeView):
+    success_url = reverse_lazy('users:profile')
+    success_message = 'パスワードの変更が完了しました'
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'エラー')
+        return redirect('users:profile')
